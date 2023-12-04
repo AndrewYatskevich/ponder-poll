5ab4636bf55dfdf63fe05545edba128bf72e6995
Andrew <drew.yatskevich@gmail.com>
2023-12-05 01:36:37 +0300

    Add details to display Poll, Option and Vote in the admin

--- ponder_poll/polls/admin.py
+++ ponder_poll/polls/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
+
 from polls.models import Option, Poll, Vote
 
-admin.site.register(Poll)
-admin.site.register(Option)
+
+class OptionInline(admin.TabularInline):
+    model = Option
+
+
+class VoteInline(admin.TabularInline):
+    model = Vote
+
+
+class PollAdmin(admin.ModelAdmin):
+    inlines = [OptionInline]
+
+
+class OptionAdmin(admin.ModelAdmin):
+    inlines = [VoteInline]
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Option, OptionAdmin)
 admin.site.register(Vote)
