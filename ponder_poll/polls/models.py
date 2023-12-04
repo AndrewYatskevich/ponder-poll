1c5e3987f77033a64ca47945ed92e6fd697ead9f
Andrew <drew.yatskevich@gmail.com>
2023-12-04 16:10:32 +0300

    Create polls endpoints

--- ponder_poll/polls/models.py
+++ ponder_poll/polls/models.py
@@ -1,32 +1,50 @@
 from django.contrib.auth import get_user_model
-from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import Count
 
 User = get_user_model()
 
 
 class Poll(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.CharField(max_length=200)
+    text = models.CharField(max_length=200, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def total_votes(self):
+        votes = Poll.objects.filter(pk=self.pk).aggregate(
+            total=Count("options__votes")
+        )
+        return votes["total"]
 
 
 class Option(models.Model):
-    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
-    text = models.CharField(max_length=200)
-    quantity = models.IntegerField(
-        validators=[MinValueValidator(0)], default=0
+    poll = models.ForeignKey(
+        Poll, on_delete=models.CASCADE, related_name="options"
     )
-
-
-class Vote(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
-    option = models.ForeignKey(Option, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=("poll", "user"), name="unique_vote"
+                fields=("poll", "text"), name="unique_option"
             )
         ]
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    option = models.ForeignKey(
+        Option, on_delete=models.CASCADE, related_name="votes"
+    )
+
+    def validate_unique_vote(self):
+        if Vote.objects.filter(
+            user=self.user, option__poll=self.option.poll
+        ).exists():
+            raise ValidationError("Vote must be unique")
+
+    def save(self, *args, **kwargs):
+        self.validate_unique_vote()
+        super().save(*args, **kwargs)
--- ponder_poll/ponder_poll/urls.py
+++ ponder_poll/ponder_poll/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include(("users.urls", "users"), namespace="users")),
+    path("", include(("polls.urls", "polls"), namespace="polls")),
 ]
